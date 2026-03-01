450aec99482f8a2e2cb9ee214ef15bd7627d15aa
Valentin Drenkov <drenkovvv@gmail.com>
2026-03-01 15:40:49 +0200

    Project name unified.

---
+++ src/main/java/dev/vdrenkov/slm/SpringLibraryManagementApplication.java
@@ -0,0 +1,15 @@
+package dev.vdrenkov.slm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLibraryManagementApplication {
+    private SpringLibraryManagementApplication() {
+        /* This utility class should not be instantiated */
+    }
+
+    public static void main(final String[] args) {
+        SpringApplication.run(SpringLibraryManagementApplication.class, args);
+    }
+}
